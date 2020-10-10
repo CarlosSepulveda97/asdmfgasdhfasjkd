3b713cb6d4845649d085f4fe8275066505d6a833
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-10-09 22:32:12 -0300

    Tres libros

--- app/src/main/java/com/example/biblioteca_app/Github_act.java
+++ app/src/main/java/com/example/biblioteca_app/Github_act.java
@@ -3,11 +3,15 @@ package com.example.biblioteca_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import Libro.Libro;
 
 public class Github_act extends AppCompatActivity {
 
@@ -19,17 +23,35 @@ public class Github_act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_github_act);
 
-        //llamo a los elementos de la interfaz
-        spin = (Spinner)findViewById(R.id.spinlibros);
-        text = (TextView)findViewById(R.id.tv1);
+        spin = (Spinner) findViewById(R.id.spinlibros);
+        text = (TextView) findViewById(R.id.tv1);
+        final Libro libro = new Libro();
 
-        //Recibo los datos de mi lista de datos del MainActivity
-        ArrayList<String> listaLibros = (ArrayList<String>) getIntent().getSerializableExtra("listalibros");
+        String[] libros = {"Farenheit", "Revival", "El Alquimista"};
 
-        //con esto relleno el spinner y le paso mi arreglo de libros
-        ArrayAdapter<String> adapt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaLibros);
+        spin.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, libros));
 
-        spin.setAdapter(adapt); // lepaso mi listado adapt
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (spin.getSelectedItem().toString().equals("El Alquimista")){
+                    text.setText("El precio del libro " + spin.getSelectedItem().toString() + " es " + libro.getAlquimista());
+                }
+                if (spin.getSelectedItem().toString().equals("Revival")){
+                    text.setText("El precio del libro " + spin.getSelectedItem().toString() + " es " + libro.getRevival() + "");
+                }
+                if (spin.getSelectedItem().toString().equals("Farenheit")){
+                    text.setText("El precio del libro " + spin.getSelectedItem().toString() + " es " + libro.getFarenheit() + "");
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/biblioteca_app/MainActivity.java
+++ app/src/main/java/com/example/biblioteca_app/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.biblioteca_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -12,6 +17,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar pb;
+    private Button btn;
+    private ImageButton ib;
 
 
     // aqui mi ciclo de trabajo
@@ -20,20 +28,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pb = (ProgressBar) findViewById(R.id.progressBar);
+        btn = (Button) findViewById(R.id.button);
+        ib = (ImageButton) findViewById(R.id.imageButton);
+
+        ib.setVisibility(View.INVISIBLE);
+        pb.setVisibility(View.INVISIBLE);
+
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                new Task().execute();
+            }
+        });
 
     }
 
+    public void goToGit(View view){
+        Intent intent = new Intent(this, Github_act.class);
+        startActivity(intent);
+    }
 
-    public void git(View v)
-    {   //lista de datos para enviar
-        ArrayList<String> libros = new ArrayList<String>();
 
-        libros.add("Farenheith");
-        libros.add("Reival");
-        libros.add("El Alquimista");
 
-        Intent i = new Intent(this, Github_act.class);
-        i.putExtra("listalibros", libros); //prepara el dato para enviar
-        startActivity(i);
+    class Task extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute(){
+            pb.setVisibility(View.VISIBLE);
+        }
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            for (int i = 1; i<10; i++){
+                try{
+                    Thread.sleep(100);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            pb.setVisibility(View.INVISIBLE);
+            ib.setVisibility(View.VISIBLE);
+        }
+
     }
 }
